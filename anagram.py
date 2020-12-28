885597f486f1960d8092472c6b08e75442d03691
Duane Deane <lilboom@Duanes-MacBook-Air.local>
2020-12-28 09:24:33 -0600

    anagram

---
+++ anagram.py
@@ -0,0 +1,8 @@
+def check(s1, s2):   
+    if(sorted(s1)== sorted(s2)): 
+        print("Anagrams, the strings are.")  
+    else: 
+        print("Anagrams, the strings aren't.")   
+s1 ="bad"
+s2 ="dsb" 
+check(s1, s2) 
